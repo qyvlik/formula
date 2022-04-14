251e3bc18a696dbc1b371282767701d021d9738e
qyvlik <qyvlik@qq.com>
2022-04-14 10:35:13 +0800

    feat: add ci

--- src/main/java/io/github/qyvlik/formula/modules/formula/service/impl/FormulaCalculator.java
+++ src/main/java/io/github/qyvlik/formula/modules/formula/service/impl/FormulaCalculator.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.udojava.evalex.Expression;
 import io.github.qyvlik.formula.common.base.AppException;
-import io.github.qyvlik.formula.common.base.Code;
 import io.github.qyvlik.formula.modules.formula.model.CalculateContext;
 import io.github.qyvlik.formula.modules.formula.model.CalculateResultData;
 import io.github.qyvlik.formula.modules.formula.model.CalculateVariable;
